dee66c703b8da267de74267ef5a5cab9fa880acb
Will Cook <wellsee83@gmail.com>
2016-03-08 16:57:26 -0600

    Consolidated questionNumber commands

--- scripts/main.js
+++ scripts/main.js
@@ -3,182 +3,167 @@ var questionNumber = 0;
 
 console.log('1. Date.UTC begins on January 1 of what year?');
 var one = prompt('1. Date.UTC begins on January 1 of what year?');
+questionNumber += 1;
 
 if (one === '1970') {
 	counter += 1;
-	questionNumber += 1;
 	console.log('Correct! :D');
 }	else {
 	console.log('Incorrect :(');
-	questionNumber += 1;
 }
 
 console.log('2. The parseFloat() function converts a string argument to return a floating point _______.');
 var two = prompt('2. The parseFloat() function converts a string argument to return a floating point _______.');
+questionNumber += 1;
 
 if (two === 'number') {
 	counter += 1;
-	questionNumber += 1;
 	console.log('Correct! :D');
 }	else {
 	console.log('Incorrect :(');
-	questionNumber += 1;
 }
 
 console.log('3. Which property is used to present text in Helvetica: font-type or font-family?');
 var three = prompt('3. Which property is used to present text in Helvetica: font-type or font-family?');
+questionNumber += 1;
 
 if (three === 'font-family') {
 	counter += 1;
-	questionNumber += 1;
 	console.log('Correct! :D');
 }	else {
 	console.log('Incorrect :(');
-	questionNumber += 1;
 }
 
 console.log('4. ":last-child" is an example of a _______.');
 var four = prompt('4. ":last-child" is an example of a _______.');
+questionNumber += 1;
 
 if (four === 'pseudo-class' || 'pseudoclass') {
 	counter += 1;
-	questionNumber += 1;
 	console.log('Correct! :D');
 }	else {
 	console.log('Incorrect :(');
-	questionNumber += 1;
 }
 
 console.log('5. Is the <span> element classified as inline or block-level?');
 var five = prompt('5. Is the <span> element classified as inline or block-level?');
+questionNumber += 1;
 
 if (five === 'inline') {
 	counter += 1;
-	questionNumber += 1;
 	console.log('Correct! :D');
 }	else {
 	console.log('Incorrect :(');
-	questionNumber += 1;
 }
 
 console.log('6. 1 em is equal to how many px?');
 var six = prompt('6. 1 em is equal to how many px?');
+questionNumber += 1;
 
 if (six === '16' || 'sixteen') {
 	counter += 1;
-	questionNumber += 1;
 	console.log('Correct! :D');
 }	else {
 	console.log('Incorrect :(');
-	questionNumber += 1;
 }
 
 console.log('7. Which is larger on the page: <h3> or <h5>?');
 var seven = prompt('7. Which is larger: <h3> or <h5>?');
+questionNumber += 1;
 
 if (seven === '<h3>' || 'h3') {
 	counter += 1;
-	questionNumber += 1;
 	console.log('Correct! :D');
 }	else {
 	console.log('Incorrect :(');
-	questionNumber += 1;
 }
 
 console.log('8. What type of operator is the "bang" (!)?');
 var eight = prompt('8. What type of operator is the "bang" (!)?');
+questionNumber += 1;
 
 if (eight === 'Unary') {
 	counter += 1;
-	questionNumber += 1;
 	console.log('Correct! :D');
 }	else {
 	console.log('Incorrect :(');
-	questionNumber += 1;
 }
 
 console.log('9. Math.floor() rounds up or down?');
 var nine = prompt('9. Math.floor() rounds up or down?');
+questionNumber += 1;
 
 if (nine === 'down' || 'rounds down') {
 	counter += 1;
-	questionNumber += 1;
 	console.log('Correct! :D');
 }	else {
 	console.log('Incorrect :(');
-	questionNumber += 1;
 }
 
 console.log('10. What symbol of a conditional operator translates to "OR"?');
 var ten = prompt('10. What symbol of a conditional operator translates to "OR"?');
+questionNumber += 1;
 
 if (ten === '||') {
 	counter += 1;
-	questionNumber += 1;
 	console.log('Correct! :D');
 }	else {
 	console.log('Incorrect :(');
-	questionNumber += 1;
 }
 
 console.log('11. What HTML element connects a document to a style sheet?');
 var eleven = prompt('11. What HTML element connects a document to a style sheet?');
+questionNumber += 1;
 
 if (eleven === '<link>' || 'link') {
 	counter += 1;
-	questionNumber += 1;
 	console.log('Correct! :D');
 }	else {
 	console.log('Incorrect :(');
-	questionNumber += 1;
 }
 
 console.log('12. How do you center text in CSS? (_______: ______;)');
 var twelve = prompt('12. How do you center text in CSS? (_______: ______;)');
+questionNumber += 1;
 
 if (twelve === 'text-align: center;' || 'text-align:center;' || 'text-align: center' || 'text-align:center') {
 	counter += 1;
-	questionNumber += 1;
 	console.log('Correct! :D');
 }	else {
 	console.log('Incorrect :(');
-	questionNumber += 1;
 }
 
 console.log('13. The class attribute can be added to "one HTML tag" or "multiple HTML tags"?');
 var thirteen = prompt('13. The class attribute can be added to "one HTML tag" or "multiple HTML tags"?');
+questionNumber += 1;
 
 if (thirteen === 'multiple HTML tags' || 'multiple') {
 	counter += 1;
-	questionNumber += 1;
 	console.log('Correct! :D');
 }	else {
 	console.log('Incorrect :(');
-	questionNumber += 1;
 }
 
 console.log('14. When centering a block element, enter "margin: 0 ____".');
 var fourteen = prompt('14. When centering a block element, enter "margin: 0 ____".');
+questionNumber += 1;
 
 if (fourteen === 'auto') {
 	counter += 1;
-	questionNumber += 1;
 	console.log('Correct! :D');
 }	else {
 	console.log('Incorrect :(');
-	questionNumber += 1;
 }
 
 console.log('15. To separate the distance between 2 lines of text, use which property - line-height or line-break?');
 var fifteen = prompt('15. To separate the distance between 2 lines of text, use which property - line-height or line-break?');
+questionNumber += 1;
 
 if (fifteen === 'line-height') {
 	counter += 1;
-	questionNumber += 1;
 	console.log('Correct! :D');
 }	else {
 	console.log('Incorrect :(');
-	questionNumber += 1;
 }
 console.log(counter);
 console.log('Your score: ' + (Math.ceil(counter/questionNumber*100)).toString() + '%!');
