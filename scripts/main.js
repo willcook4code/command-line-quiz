852604d4e8e9c5e06fbd15c3d8ac98e59d4a94db
Will Cook <wellsee83@gmail.com>
2016-03-08 18:00:03 -0600

    Wrapped all groups of || in parenthesis

--- scripts/main.js
+++ scripts/main.js
@@ -27,7 +27,7 @@ console.log('3. Which property is used to present text in Helvetica: font-type o
 var three = prompt('3. Which property is used to present text in Helvetica: font-type or font-family?');
 questionNumber += 1;
 
-if (three === 'font-family') {
+if (three.toLowerCase() === 'font-family') {
 	counter += 1;
 	console.log('Correct! :D');
 }	else {
@@ -38,7 +38,7 @@ console.log('4. ":last-child" is an example of a _______.');
 var four = prompt('4. ":last-child" is an example of a _______.');
 questionNumber += 1;
 
-if (four === ('pseudo-class' || 'pseudoclass')) {
+if ((four.toLowerCase() === 'pseudo-class') || (four.toLowerCase() === 'pseudoclass')) {
 	counter += 1;
 	console.log('Correct! :D');
 }	else {
@@ -49,7 +49,7 @@ console.log('5. Is the <span> element classified as inline or block-level?');
 var five = prompt('5. Is the <span> element classified as inline or block-level?');
 questionNumber += 1;
 
-if (five === 'inline') {
+if (five.toLowerCase() === 'inline') {
 	counter += 1;
 	console.log('Correct! :D');
 }	else {
@@ -60,7 +60,7 @@ console.log('6. 1 em is equal to how many px?');
 var six = prompt('6. 1 em is equal to how many px?');
 questionNumber += 1;
 
-if (six === ('16' || 'sixteen')) {
+if ((six.toLowerCase() === '16') || (six.toLowerCase() === 'sixteen')) {
 	counter += 1;
 	console.log('Correct! :D');
 }	else {
@@ -71,7 +71,7 @@ console.log('7. Which is larger on the page: <h3> or <h5>?');
 var seven = prompt('7. Which is larger: <h3> or <h5>?');
 questionNumber += 1;
 
-if (seven === ('<h3>' || 'h3')) {
+if ((seven.toLowerCase() === '<h3>') || (seven.toLowerCase() ===  'h3')) {
 	counter += 1;
 	console.log('Correct! :D');
 }	else {
@@ -82,7 +82,7 @@ console.log('8. What type of operator is the "bang" (!)?');
 var eight = prompt('8. What type of operator is the "bang" (!)?');
 questionNumber += 1;
 
-if (eight === 'Unary') {
+if (eight.toLowerCase() === 'unary') {
 	counter += 1;
 	console.log('Correct! :D');
 }	else {
@@ -93,7 +93,7 @@ console.log('9. Math.floor() rounds up or down?');
 var nine = prompt('9. Math.floor() rounds up or down?');
 questionNumber += 1;
 
-if (nine === ('down' || 'rounds down')) {
+if ((nine.toLowerCase() === 'down') || (nine.toLowerCase() ===  'rounds down')) {
 	counter += 1;
 	console.log('Correct! :D');
 }	else {
@@ -104,7 +104,7 @@ console.log('10. What symbol of a conditional operator translates to "OR"?');
 var ten = prompt('10. What symbol of a conditional operator translates to "OR"?');
 questionNumber += 1;
 
-if (ten === '||') {
+if (ten.toLowerCase() === '||') {
 	counter += 1;
 	console.log('Correct! :D');
 }	else {
@@ -115,7 +115,7 @@ console.log('11. What HTML element connects a document to a style sheet?');
 var eleven = prompt('11. What HTML element connects a document to a style sheet?');
 questionNumber += 1;
 
-if (eleven === ('<link>' || 'link')) {
+if ((eleven.toLowerCase() === '<link>') || (eleven.toLowerCase() === 'link')) {
 	counter += 1;
 	console.log('Correct! :D');
 }	else {
@@ -126,7 +126,7 @@ console.log('12. How do you center text in CSS? (_______: ______;)');
 var twelve = prompt('12. How do you center text in CSS? (_______: ______;)');
 questionNumber += 1;
 
-if (twelve === ('text-align: center;' || 'text-align:center;' || 'text-align: center' || 'text-align:center')) {
+if ((twelve.toLowerCase() === 'text-align: center;') || (twelve.toLowerCase() === 'text-align:center;') || (twelve.toLowerCase() === 'text-align: center') || (twelve.toLowerCase() === 'text-align:center')) {
 	counter += 1;
 	console.log('Correct! :D');
 }	else {
@@ -137,7 +137,7 @@ console.log('13. The class attribute can be added to "one HTML tag" or "multiple
 var thirteen = prompt('13. The class attribute can be added to "one HTML tag" or "multiple HTML tags"?');
 questionNumber += 1;
 
-if (thirteen === ('multiple HTML tags' || 'multiple')) {
+if ((thirteen.toLowerCase() === 'multiple HTML tags') || (thirteen.toLowerCase() === 'multiple')) {
 	counter += 1;
 	console.log('Correct! :D');
 }	else {
@@ -148,7 +148,7 @@ console.log('14. When centering a block element, enter "margin: 0 ____".');
 var fourteen = prompt('14. When centering a block element, enter "margin: 0 ____".');
 questionNumber += 1;
 
-if (fourteen === 'auto') {
+if (fourteen.toLowerCase() === 'auto') {
 	counter += 1;
 	console.log('Correct! :D');
 }	else {
@@ -159,12 +159,13 @@ console.log('15. To separate the distance between 2 lines of text, use which pro
 var fifteen = prompt('15. To separate the distance between 2 lines of text, use which property - line-height or line-break?');
 questionNumber += 1;
 
-if (fifteen === 'line-height') {
+if (fifteen.toLowerCase() === 'line-height') {
 	counter += 1;
 	console.log('Correct! :D');
 }	else {
 	console.log('Incorrect: line-height');
 }
+
 console.log(counter);
 console.log('Your score: ' + (Math.ceil(counter/questionNumber*100)).toString() + '%!');
 
